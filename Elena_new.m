7d6d332aec1fb7b77680bcb5182eb486c89d274e
Unknown <wihelmina14@yahoo.com>
2018-04-11 18:07:42 +0200

    11-04-2018

--- Elena_new.m
+++ Elena_new.m
@@ -1,26 +1,27 @@
  % CASE = 0         Original problem
- % CASE = 1         A --> AB', w --> p, t --> q
- % CASE = 2         x = B'u
+ % CASE = 1         x = B'u
  % CASE = 3         Permutation
  
-   CASE = 1;        % <--- CHANGE THIS ONE
+ CASE = 1;        % <--- CHANGE THIS ONE
  
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  
-
+%Dimesions
 n=size(A,1);
 k=size(A,2);
 
+%Create diagonal matrix to change signs
 D = diag(e);
 
+%Change 
 DA=D*A;
 I_n=eye(n);
 O_k=zeros(k,k);
 O_n_k=zeros(n,k);
 One_n1=ones(n,1);
 O_n1=zeros(n,1);
-O_nk=zeros(n+k,1);
-O_n=zeros(n,n);
+O_nk=zeros(k+1,1);
+O_n=zeros(n,1);
 One_n=ones(n,1);
 
 if CASE == 0
@@ -28,10 +29,10 @@ if CASE == 0
 % CASE 0                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-E = [ -DA   -D   -I_n; ...
-      O_n_k   O_n  -I_n];
+E = [ -DA   -e   -I_n; ...
+      O_n_k  O_n  -I_n];
   
-b = [ -One_n1; ...
+r = [ -One_n1; ...
       O_n1];
  
   
@@ -40,13 +41,13 @@ c = [ O_nk;...
   
 options = optimoptions('linprog','Algorithm','interior-point-legacy ','Display','iter');
 
-[x,f,exitflag,out,lambda] = linprog(c, E, b,[],[],[],[],options);
+[x,f,exitflag,out,lambda] = linprog(c, E, r,[],[],[],[],options);
 
  %%% Finding private coefficients
      try
-        w = x(k+n+1:end);
+        w = x(k+2:end);
         a = x(1:k);
-        b = x(k+1:k+n);
+        b = x(k+1);
         f = c'*x;             
      catch
         fprintf(2, 'PROBLEMS HAVE OCCURED. CHECK x\n');
@@ -56,31 +57,31 @@ elseif CASE == 1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CASE 1                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-E = [ -DA   -D   -I_n; ...
+E = [ -DA   -e   -I_n; ...
       O_n_k   O_n  -I_n];
 
   
-b = [ -One_n1; ...
+r = [ -One_n1; ...
       O_n1];
  
   
 c = [ O_nk;...
       One_n];
   
-B=rand(2*n+k,2*n+k);
+B=randi(10,n+k+1,n+k+1);
 
 EE=E*B';
 cc=c'*B';
 
 options = optimoptions('linprog','Algorithm','interior-point-legacy ','Display','iter');
-[u,f,exitflag,out,lambda] = linprog(cc, EE, b,[],[],[],[],options);
+[u,f,exitflag,out,lambda] = linprog(cc, EE, r,[],[],[],[],options);
 
  %%% Finding private coefficients
      try
         x=B'*u;
-        w = x(k+n+1:end);
+        w = x(k+2:end);
         a = x(1:k);
-        b = x(k+1:k+n);
+        b = x(k+1);
         f = c'*x;             
      catch
         fprintf(2, 'PROBLEMS HAVE OCCURED. CHECK x\n');
