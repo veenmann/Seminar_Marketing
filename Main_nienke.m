ea2ca47e53b2f65a8e716d9d85f51036100be5f8
Unknown <wihelmina14@yahoo.com>
2018-04-12 11:42:28 +0200

    from nienke with b number

---
+++ Main_nienke.m
@@ -0,0 +1,86 @@
+function [a,b]=Main_nienke(V,e) 
+ % CASE = 0         Original problem
+ % CASE = 1         A --> AB', w --> p, t --> q
+ % CASE = 2         x = B'u
+ % CASE = 3         Permutation
+ 
+   CASE = 0;        % <--- CHANGE THIS ONE
+ 
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
+
+n=size(V,1);
+k=size(V,2);
+
+D = diag(e);
+S = e;
+
+DV = D*V;
+I_n=eye(n);
+O_n_k=zeros(n,k);
+O_n_1=zeros(n,1);
+O_k1_1 = zeros(k+1,1);
+One_n_1=ones(n,1);
+
+if CASE == 0
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CASE 0                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+E = [ -DV    -S   -I_n; ...
+      O_n_k  O_n_1  -I_n];
+  
+upperbound = [ -One_n_1; ...
+                 O_n_1];
+ 
+  
+c = [ O_k1_1;...
+      One_n_1];
+  
+options = optimoptions('linprog','Algorithm','interior-point-legacy ','Display','iter');
+
+[x,f,exitflag,out,lambda] = linprog(c, E, upperbound,[],[],[],[],options);
+
+ %%% Finding private coefficients
+     try
+        u = x(k+2:end);
+        a = x(1:k);
+        b = x(k+1);
+        f = c'*x;             
+     catch
+        fprintf(2, 'PROBLEMS HAVE OCCURED. CHECK x\n');
+     end
+
+elseif CASE == 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CASE 1                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+E = [ -DV    -S   -I_n; ...
+      O_n_k  O_n_1  -I_n];
+  
+upperbound = [ -One_n_1; ...
+                 O_n_1];
+ 
+  
+c = [ O_k1_1;...
+      One_n_1];
+  
+B=rand(k+1+n,k+1+n);
+
+EE=E*B';
+cc=c'*B';
+
+options = optimoptions('linprog','Algorithm','interior-point-legacy ','Display','iter');
+[w,f,exitflag,out,lambda] = linprog(cc, EE, upperbound,[],[],[],[],options);
+
+ %%% Finding private coefficients
+     try
+        x=B'*w;
+        u = x(k+2:end);
+        a = x(1:k);
+        b = x(k+1);
+        f = c'*x;             
+     catch
+ fprintf(2, 'PROBLEMS HAVE OCCURED. CHECK x\n');
+     end
+end
